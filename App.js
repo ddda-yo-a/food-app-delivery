ab5990fec71eb91f47fe764d14f1cf3c94a0b104
Chinedu Caleb Obodeh <coredevhub@Chinedus-MacBook-Air.local>
2026-06-10 15:50:35 -0700

    boilerplate

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+import "./global.css"
+
+SplashScreen.preventAutoHideAsync();
+
+const customFonts = {
+  "Quicksand-Bold": require("./assets/fonts/Quicksand-Bold.ttf"),
+  "Quicksand-Regular": require("./assets/fonts/Quicksand-Regular.ttf"),
+  "Quicksand-Medium": require("./assets/fonts/Quicksand-Medium.ttf"),
+  "Quicksand-SemiBold": require("./assets/fonts/Quicksand-SemiBold.ttf"),
+  "Quicksand-Light": require("./assets/fonts/Quicksand-Light.ttf"),
+};
 
 export default function App() {
+  const [fontsLoaded, error] = useFonts(customFonts);
+
+  useEffect(() => {
+    if (fontsLoaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (error) {
+    throw error;
+  }
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-5xl text-center font-bold text-primary" style={{ fontFamily: "Quicksand-Bold" }}>
+
+    {/* style={{ fontFamily: "Quicksand-Bold" }} */}
+        Welcome to Nativewind!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
